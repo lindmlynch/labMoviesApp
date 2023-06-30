da23109a2ecacdc5df808c49fd20e9efe4ca75d4
Linda Lynch <lindmlynch@gmail.com>
2023-06-30 10:04:12 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
 import { getUpcomingMovies } from '../api/tmdb-api';
+import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'; 
 
 const UpcomingMoviesPage = () => {
-  const [movies, setMovies] = useState([]);
+  const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies);
 
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-  }, []);
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data ? data : [];
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
+      action={(movie) => <AddToFavouritesIcon movie={movie} />} 
     />
   );
 };
